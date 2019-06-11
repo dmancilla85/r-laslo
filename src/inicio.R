73f61fc67b5e8a7d20c1763bf2d0228aeb1dd6bc
dmancilla85 <david.a.m@live.com>
2019-06-10 21:06:53 -0300

    10-06-2019

--- src/inicio.R
+++ src/inicio.R
@@ -1,61 +1,23 @@
 # Cargar framework
 source("./src/configuration.R")
 
-# Cargar datos
-df <- loadFiles(path="./data/fruitfly_bound/", pattern = "*.csv")
+######
 
-#Eliminar variables innecesarias
-df <- formatEnsembl(df)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 
-# a. correlaciones
-getCors(df)
-plotBaseDistribution(df)
-df$Serie <- factor(df$Serie)
+BiocManager::install("annotate")
 
-ggdensity(df, x = "RnaFoldMFE",
-          add = "mean", rug = TRUE,
-          color = "Serie", fill = "Serie",
-          palette = c("#00AFBB", "#E7B800"))
+library(annotate)
 
-ggplot(df[df$Serie==0,], aes(x=Pairments)) +
-  geom_histogram()
+xdoc <- genbank("FBgn0023537", type="accession",disp="data")
+class(xdoc)
 
+lst <- xmlToList(xdoc)
+lst <- unlist(lst)
 
-#################
-summary(df)
-df$RnaFoldMFE <- cut(df$RnaFoldMFE, seq(-20, 0, 5))
+ix <- str_detect(lst, "NM_")
+res <- lst[ix]
+res %>% unique()
 
-library(rpart)
-df.fit <- rpart(Serie ~ RelativePosition + RnaFoldMFE + PurinePercentStem +
-                  + CG_PercentPairs + AU_PercentPairs + GU_PercentPairs +
-                  WooblePairs + Pairments + Bulges + InternalLoops +
-                  N.2 + N.1 + N2 + N5 + N6 + N7 + N8 + TerminalPair +
-                  LoopPattern + Chromosome, data=df, method="class")
-
-plotcp(df.fit)
-
-plot(df.fit, uniform=TRUE, 
-     main="Classification Tree for Kyphosis")
-text(df.fit, use.n=FALSE, all=TRUE, cex=.8)
-
-# 
-# newdata <- data.frame(RelativePosition= , 
-#                       RnaFoldMFE= , 
-#                       PurinePercentStem= ,
-#                       CG_PercentPairs= ,
-#                       AU_PercentPairs= , 
-#                       GU_PercentPairs= ,
-#                       WooblePairs= , 
-#                       Pairments= ,
-#                       Bulges= ,
-#                       InternalLoops= ,
-#                       N.2= ,
-#                       N.1= ,
-#                       N2= ,
-#                       N5= ,
-#                       N6= ,
-#                       N7= ,
-#                       N8= ,
-#                       TerminalPair= ,
-#                       LoopPattern= ,
-#                       Chromosome = )
+######
